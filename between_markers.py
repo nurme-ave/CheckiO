a24e6b917f714bb30f7ce1b5c9423f1581e5c863
Ave Nurme <nurme.ave@gmail.com>
2020-05-08 16:29:20 +0300

    Update between_markers.py

--- between_markers.py
+++ between_markers.py
@@ -1,10 +1,14 @@
-def between_markers(text: str, begin: str, end: str) -> str:
+ def between_markers(text: str, begin: str, end: str) -> str:
     """
-        returns substring between two given markers
+    You are given a string and two markers (the initial and final).
+    You have to find a substring enclosed between these two markers. But there are a few important conditions:
+    - the initial and final markers are always different
+    - if there is no initial marker, then the first character should be considered the beginning of a string
+    - if there is no final marker, then the last character should be considered the ending of a string
+    - if the initial and final markers are missing then simply return the whole string
+    - if the final marker comes before the initial marker, then return an empty string
     """
     index_begin, index_end = text.index(begin), text.index(end)
-    if index_end - index_end == 1:
-        return '" "'
     return text[index_begin + 1 : index_end]
 
 
