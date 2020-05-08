0513fb14270496d0c986031c188e6838a00f89fb
Ave Nurme <nurme.ave@gmail.com>
2020-05-08 17:08:38 +0300

    Add files via upload

---
+++ second_index.py
@@ -0,0 +1,18 @@
+def second_index(text: str, symbol: str) -> [int, None]:
+    """
+        returns the second index of a symbol in a given text
+    """
+    pos = [pos for pos, char in enumerate(text) if char == symbol]
+    if len(pos) >= 2:
+        return pos[1]
+    return None
+
+
+if __name__ == '__main__':
+    print('Example:')
+    print(second_index("sims", "s"))
+    print(second_index("find the river", "e"))
+    print(second_index("hi", " "))
+    print(second_index("hi mayor", " "))
+    print(second_index("hi mr Mayor", " "))
+    print(second_index("three occurrences", "r"))
