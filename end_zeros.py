d388789bd1d0761cfe869ef4ef6d93523917f2ed
Ave Nurme <nurme.ave@gmail.com>
2020-05-08 16:44:49 +0300

    Add files via upload

---
+++ end_zeros.py
@@ -0,0 +1,18 @@
+def end_zeros(num: int) -> int:
+    count = 0
+    num = str(num)
+    for n in num[::-1]:
+        if n == '0':
+            count += 1
+        else:
+            break
+
+    return count
+
+
+print(end_zeros(0))     # == 1
+print(end_zeros(1))     # == 0
+print(end_zeros(10))    # == 1
+print(end_zeros(101))   # == 0
+print(end_zeros(245))   # == 0
+print(end_zeros(100100))    # == 2
