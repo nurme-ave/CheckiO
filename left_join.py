82c50f72d7e126672768bd8e2a604de428266389
Ave Nurme <nurme.ave@gmail.com>
2020-05-08 16:56:02 +0300

    Update left_join.py

--- left_join.py
+++ left_join.py
@@ -1,7 +1,5 @@
 def left_join(phrases):
-    """
-        Join strings and replace "right" to "left"
-    """
+    """Join strings and replace "right" to "left"."""
     new_string = ''
 
     for word in phrases:
@@ -12,7 +10,7 @@ def left_join(phrases):
 
 if __name__ == '__main__':
     print('Example:')
-    print(left_join(("left", "right", "left", "stop")))
-    print(left_join(("bright aright", "ok")))
-    print(left_join(("brightness wright",)))
-    print(left_join(("enough", "jokes")))
+    print(left_join(("left", "right", "left", "stop"))) # left,left,left,stop
+    print(left_join(("bright aright", "ok")))           # bleft aleft,ok
+    print(left_join(("brightness wright",)))            # bleftness wleft
+    print(left_join(("enough", "jokes")))               # enough,jokes
