d0c3c5ef1b4b98b08d40f35a14c4fb8990b2260c
Ave Nurme <nurme.ave@gmail.com>
2020-05-08 17:14:04 +0300

    Update split_list.py

--- split_list.py
+++ split_list.py
@@ -2,6 +2,10 @@ from math import ceil
 
 
 def split_list(items: list) -> list:
+    """
+    Split a given array into two arrays. If it has an odd amount of elements, then the first array should have more elements.
+    If it has no elements, then two empty arrays should be returned.
+    """
     if len(items) % 2 == 0:
         half_of_items = len(items) // 2
         first_half, second_half = items[:half_of_items], items[half_of_items:]
