1965b928778db8f2e4426d73bda36cecc1c383de
Ave Nurme <nurme.ave@gmail.com>
2020-05-08 21:46:01 +0300

    Update three_words.py

--- three_words.py
+++ three_words.py
@@ -1,4 +1,4 @@
-def checkio(words: str) -> bool:
+def three_words(words: str) -> bool:
     """
     You are given a string with words and numbers separated by whitespaces (one space).
     The words contains only letters. This program checks if the string contains three words in succession.
@@ -19,9 +19,9 @@ def checkio(words: str) -> bool:
     return False
 
 
-print(checkio("Hello World hello"))  # == True, "Hello"
-print(checkio("He is 123 man"))  # == False, "123 man"
-print(checkio("1 2 3 4"))  # == False, "Digits"
-print(checkio("bla bla bla bla"))  # == True, "Bla Bla"
-print(checkio("Hi"))  # == False, "Hi"
-print(checkio("one two 3 four five six 7 eight 9 ten eleven 12"))  # == True
+print(three_words("Hello World hello"))  # == True, "Hello"
+print(three_words("He is 123 man"))  # == False, "123 man"
+print(three_words("1 2 3 4"))  # == False, "Digits"
+print(three_words("bla bla bla bla"))  # == True, "Bla Bla"
+print(three_words("Hi"))  # == False, "Hi"
+print(three_words("one two 3 four five six 7 eight 9 ten eleven 12"))  # == True
