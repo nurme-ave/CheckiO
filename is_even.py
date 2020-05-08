78945da746837c786e482417f5447d18760dc2fb
Ave Nurme <nurme.ave@gmail.com>
2020-05-08 21:36:11 +0300

    Add files via upload

---
+++ is_even.py
@@ -0,0 +1,9 @@
+
+def is_even(num: int) -> bool:
+    """Check is the given number is even or not."""
+    return num % 2 == 0
+
+
+print(is_even(2))   # True
+print(is_even(5))   # False
+print(is_even(0))   # True
