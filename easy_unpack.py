74ca7c0d6793c5c28e4fe2aee08c1e9326d4ed4a
Ave Nurme <nurme.ave@gmail.com>
2020-05-08 16:43:48 +0300

    Update easy_unpack.py

--- easy_unpack.py
+++ easy_unpack.py
@@ -1,10 +1,8 @@
 def easy_unpack(elements: tuple) -> tuple:
-    """
-        Returns a tuple with 3 elements - first, third and second to the last.
-    """
+    """Returns a tuple with 3 elements - first, third and second to the last."""
     return elements[0], elements[2], elements[-2]
 
-print(easy_unpack((1, 2, 3, 4, 5, 6, 7, 9)))
+
 print(easy_unpack((1, 2, 3, 4, 5, 6, 7, 9)) == (1, 3, 7))
 print(easy_unpack((1, 1, 1, 1)) == (1, 1, 1))
 print(easy_unpack((6, 3, 7)) == (6, 7, 3))
