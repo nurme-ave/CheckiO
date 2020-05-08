a8d6eb4d13922bba60ef685e4a139821352a821c
Ave Nurme <nurme.ave@gmail.com>
2020-05-08 17:16:50 +0300

    Add files via upload

---
+++ the_most_numbers.py
@@ -0,0 +1,14 @@
+def most_numbers(*args):
+    if len(args) > 1:
+        if min(args) < 0 and max(args) < 0:
+            return -(abs(max(args)) - abs(min(args)))
+        return abs(max(args)) - min(args)
+    return 0
+
+
+print((most_numbers(1, 2, 3)))  # , 2, 3), "3-1=2"
+print((most_numbers(5, -5)))  # , 10, 3), "5-(-5)=10"
+print((most_numbers(10.2, -2.2, 0, 1.1, 0.5)))  # , 12.4, 3), "10.2-(-2.2)=12.4"
+print((most_numbers()))  # , 0, 3), "Empty"
+print(most_numbers(-0.07))
+print(most_numbers(-0.036, -0.11, -0.55, -64))
