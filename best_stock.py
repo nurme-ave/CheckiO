c1b34cdf9bc55f64d972985bab58b0142b7975d3
Ave Nurme <nurme.ave@gmail.com>
2020-05-08 16:26:06 +0300

    Update best_stock.py

--- best_stock.py
+++ best_stock.py
@@ -1,10 +1,11 @@
 
 def best_stock(a):
+    """Check which stocks cost more."""
     max_value = max(a.values())
     for key, val in a.items():
         if val == max_value:
             return key
 
 
-print(best_stock({"CAC": 10.0, "ATX": 390.2, "WIG": 1.2}))
-print(best_stock({"CAC": 91.1, "ATX": 1.01, "TASI": 120.9}))
+print(best_stock({"CAC": 10.0, "ATX": 390.2, "WIG": 1.2}))    # ATX
+print(best_stock({"CAC": 91.1, "ATX": 1.01, "TASI": 120.9}))    # TASI
