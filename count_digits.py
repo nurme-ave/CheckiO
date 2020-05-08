161f1652749f23e1f9604d3bec852231e1f7bca0
Ave Nurme <nurme.ave@gmail.com>
2020-05-08 16:35:46 +0300

    Add files via upload

---
+++ count_digits.py
@@ -0,0 +1,16 @@
+def count_digits(text: str) -> int:
+    count = 0
+    for char in text:
+        if char.isdigit():
+            count += 1
+    return count
+
+
+print(count_digits('hi'))   # 0
+print(count_digits('who is 1st here'))  # 1
+print(count_digits('my numbers is 2'))  # 1
+print(count_digits('This picture is an oil on canvas '
+ 'painting by Danish artist Anna '
+ 'Petersen between 1845 and 1910 year'))    # 8
+print(count_digits('5 plus 6 is'))  # 2
+print(count_digits(''))   # 0
