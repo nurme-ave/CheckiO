4b49caddd312d7549c7705bd46aee9c740025d97
Ave Nurme <nurme.ave@gmail.com>
2020-05-08 16:15:53 +0300

    Add files via upload

---
+++ backward_string.py
@@ -0,0 +1,10 @@
+def backward_string(val: str) -> str:
+
+    return val[::-1] if val else val
+
+
+
+print(backward_string('val'))   # == 'lav'
+print(backward_string(''))  # == ''
+print(backward_string('ohho'))  # == 'ohho'
+print(backward_string('123456789'))     # == '987654321'
