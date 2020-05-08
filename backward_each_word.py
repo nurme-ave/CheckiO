554a50d2c118194790cd76009d4cb9d862e229dd
Ave Nurme <nurme.ave@gmail.com>
2020-05-08 16:14:34 +0300

    Add files via upload

---
+++ backward_each_word.py
@@ -0,0 +1,12 @@
+def backward_string_by_word(text: str) -> str:
+    text = list(text.split(" "))
+    result = [word[::-1] for word in text]
+
+    return " ".join(result)
+
+
+print(backward_string_by_word(''))  # == ''
+print(backward_string_by_word('world'))     # == 'dlrow'
+print(backward_string_by_word('hello world'))   # == 'olleh dlrow'
+print(backward_string_by_word('hello   world'))     # == 'olleh   dlrow'
+print(backward_string_by_word('welcome to a game'))     # == 'emoclew ot a emag'
