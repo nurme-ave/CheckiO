fd41e0866cabdde69a69d956f649e52dd8e198e2
Ave Nurme <nurme.ave@gmail.com>
2020-06-07 00:27:05 +0300

    Improved the code with a better solution.

--- all_the_same.py
+++ all_the_same.py
@@ -1,8 +1,14 @@
 def all_the_same(elements) -> bool:
-    """Check if all elements in the given list are equal."""
+    """Check if all elements in the given list are equal.
+    
+    Previous version:
     if len(elements) < 1:
         return True
     return len(elements) == elements.count(elements[0])
+    """
+    
+    # New version:
+    return True if len(elements) < 1 else (len(elements) == elements.count(elements[0]))
 
 
 print(all_the_same([1, 1, 1]))  # == True
