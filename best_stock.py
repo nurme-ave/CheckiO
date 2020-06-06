62444dba637bf72ec2284f6706ca52896f1e7d4c
Ave Nurme <nurme.ave@gmail.com>
2020-06-06 21:22:09 +0300

    Learned the concept of lambdas and rewrote the solution.

--- best_stock.py
+++ best_stock.py
@@ -1,9 +1,7 @@
 def best_stock(a):
     """Check which stocks cost more."""
-    max_value = max(a.values())
-    for key, val in a.items():
-        if val == max_value:
-            return key
+    
+    return max(a, key=lambda key: a[key])
 
 
 print(best_stock({"CAC": 10.0, "ATX": 390.2, "WIG": 1.2}))    # ATX
