7c986f2951da40525176defa3df898ad45ae3817
Ave Nurme <nurme.ave@gmail.com>
2020-06-07 01:33:00 +0300

    Improved the code with a better solution.

--- replace_first.py
+++ replace_first.py
@@ -1,14 +1,17 @@
 from typing import Iterable
 
+"""
+Previous solution:
 
-def replace_first(items: list) -> Iterable:
-    """
-    In a given list the first element should become the last one.
-    An empty list or list with only one element should stay the same.
-    """
     if len(items) > 1:
         return items[1::] + items[0:1]
     return items
+"""
+
+
+# New solution
+def replace_first(items: list) -> Iterable:
+    return (items[1::] + items[0:1]) if len(items) > 1 else items
 
 
 print(replace_first([1, 2, 3, 4]))   # == [2, 3, 4, 1]
