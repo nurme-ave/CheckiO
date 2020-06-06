62a09d2abf750309bcf3569452a6c49d9a29d373
Ave Nurme <nurme.ave@gmail.com>
2020-06-06 21:24:44 +0300

    Learned the concept of lambdas and rewrote the solution.

--- best_stock.py
+++ best_stock.py
@@ -3,6 +3,14 @@ def best_stock(a):
     
     return max(a, key=lambda key: a[key])
 
+"""
+Previous solution:
+
+    max_value = max(a.values())
+    for key, val in a.items():
+        if val == max_value:
+            return key
+"""
 
 print(best_stock({"CAC": 10.0, "ATX": 390.2, "WIG": 1.2}))    # ATX
 print(best_stock({"CAC": 91.1, "ATX": 1.01, "TASI": 120.9}))    # TASI
