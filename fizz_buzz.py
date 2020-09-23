ef1307bf6f34e2b4747a714698a8f327deea741c
Ave Nurme <nurme.ave@gmail.com>
2020-09-23 18:32:01 +0300

    Replaced with a more elegant solution

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,23 +1,23 @@
 def checkio(number: int) -> str:
-    """
-    Return:
-    - "Fizz Buzz" if the number is divisible by 3 and by 5
-    - "Fizz" if the number is divisible by 3
-    - "Buzz" if the number is divisible by 5
-    """
-    if number % 3 == 0 and number % 5 == 0:
-        return 'Fizz Buzz'
-    elif number % 3 == 0:
-        return 'Fizz'
-    elif number % 5 == 0:
-        return 'Buzz'
-    else:
-        return str(number)
+
+    fizzes_buzzes = {
+        3: "Fizz",
+        5: "Buzz",
+    }
+
+    result = ""
+
+    for key, value in fizzes_buzzes.items():
+        if number % key == 0:
+            result += value
+
+    return result if result else f"{number} not divisible either by 3 or 5."
 
 
 print(checkio(3))
 print(checkio(5))
 print(checkio(15))
+print(checkio(7))
 print(checkio(15) == "Fizz Buzz", "15 is divisible by 3 and 5")
 print(checkio(6) == "Fizz", "6 is divisible by 3")
 print(checkio(5) == "Buzz", "5 is divisible by 5")
